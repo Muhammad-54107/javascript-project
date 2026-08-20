5a5814a539a0d180aba1731e88ddda51889e7ae9
Muhammad-54107 <74154378+Muhammad-54107@users.noreply.github.com>
2026-08-20 10:24:15 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code ad newer version 
+
+// alert( 3 + 3) // we are using nodejs, not browser 
+
+console.log(3 + 3); //code readbility 
+console.log("Billy") // we eneded code readilibity here // dont write all same code in same line. 
+// code readbility matters 
+
+let name = "billy";
+let age = 18
+let isLoggedIn = false
+
+// premitive dataTypes: 
+//number => 2 to power 53
+//bigint using for big numbers 
+//string => ""
+//boolean => true/false
+//null its a stand alone value //null is representation of empty value 
+//undefined => when you ddint defined any value // or value not assiend 
+//symbol => unique 
+
+//object 
+
+console.log(typeof "Billy")
+//typeof give type of value, like string 
+
+//null is Oject 
